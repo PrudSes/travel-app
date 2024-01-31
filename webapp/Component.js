ea3a2833fd687a713d9d31b810e1b531d8e0a190
Prudhvi Bhavisetty <bhavisettyprudhvi@gmail.com>
2024-01-31 23:40:39 +0000

    Sync Commit @1/31/2024, 11:40:39 PM

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zrapprud.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
